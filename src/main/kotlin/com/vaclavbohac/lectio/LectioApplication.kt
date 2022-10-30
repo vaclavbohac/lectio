3e1e1dc868afc4d21b5ea0e1c8122ee35434e613
vaclavbohac <vaclav.bohac@productboard.com>
2022-10-30 16:10:02 +0100

    Add spring boot web module

--- src/main/kotlin/com/vaclavbohac/lectio/LectioApplication.kt
+++ src/main/kotlin/com/vaclavbohac/lectio/LectioApplication.kt
@@ -2,9 +2,15 @@ package com.vaclavbohac.lectio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
-class LectioApplication
+class LectioApplication {
+    @RequestMapping("/")
+    fun home() = "Hello World!"
+}
 
 fun main(args: Array<String>) {
 	runApplication<LectioApplication>(*args)
